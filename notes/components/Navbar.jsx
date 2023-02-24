e0bd7dcfa9e2f95a35085ac54141a975470a9411
Harshit Kumar <93194961+HarshitKumar9030@users.noreply.github.com>
2023-02-24 15:45:59 +0000

    enough for today

--- notes/components/Navbar.jsx
+++ notes/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
 
 export default function Navbar() {
     const router = useRouter();
+    const [modal , setModal] = useState(false);
     
     const newNote = () => {
-        router.push("/notes/new");
+        setModal(!modal);
     }
 
     return (
@@ -14,11 +16,14 @@ export default function Navbar() {
     <>
      <div className="h-20 p-8 text-white flex justify-between items-center">
         <div className="title text-4xl md:text-6xl font-semibold">Notes</div>
-        <div className="button px-4 py-2 hover:shadow-xl shadow-zinc-700 text-white bg-zinc-800 text-xl font-medium hover:bg-zinc-700 duration-200 hover:scale-105 selection:bg-transparent cursor-ne-resize rounded-md">Add New</div>
+        <div onClick={()=> newNote()} className="button px-4 py-2 hover:shadow-xl shadow-zinc-700 text-white bg-zinc-800 text-xl font-medium hover:bg-zinc-700 duration-200 hover:scale-105 selection:bg-transparent cursor-ne-resize rounded-md">Add New</div>
      </div>
         <div className="md:mt-8 mt-6 flex justify-center">
           <div className="border w-screen mx-16 border-zinc-800"></div>
         </div>
+        {modal && (
+            hey
+        )}
     </>
   )
 }
